5678b21c24a013fd47265fa9cb9ac012b9ebf0b4
jeremiahmontebon <jeremiah.montebon@cit.edu>
2024-05-11 14:02:37 +0800

    Add code guide

--- train.py
+++ train.py
@@ -8,7 +8,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
 import joblib
-
+#
+#
+### Preprocess the text data ###
+#
+#
 # Download NLTK resources if not already downloaded
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -30,11 +34,19 @@ def preprocess_text(text):
 
 X = imdb_data['review'].apply(preprocess_text)
 y = imdb_data['sentiment']
-
+#
+#
+### Feature engineering using TF-IDF ###
+#
+#
 # Feature extraction using Bag-of-Words
 vectorizer = CountVectorizer()
 X_vectorized = vectorizer.fit_transform(X)
-
+#
+#
+### Train the model using Multinomial Naive Bayes ###
+#
+#
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X_vectorized, y, test_size=0.2, random_state=42)
 
@@ -45,7 +57,11 @@ classifier.fit(X_train, y_train)
 # Export the trained model
 joblib.dump(classifier, 'sentiment_analysis_model.joblib')
 joblib.dump(vectorizer, 'veectorizer.joblib')
-
+#
+#
+### Hyperparameter tuning and model evaluation ###
+#
+#
 # Make predictions on test data
 y_pred = classifier.predict(X_test)
 
